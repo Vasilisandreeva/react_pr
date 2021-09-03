c71504d5d2af8e05f6b288fbda73d1db907edb53
Vasya <vasya@gmail.com>
2021-09-03 14:10:04 +0300

    qqq

--- src/component/posts/Posts.js
+++ src/component/posts/Posts.js
@@ -1,5 +1,12 @@
+import {useEffect, useState} from "react";
+import {getPosts} from "../../services/posts.service";
+
 export default function Posts() {
-    let
+    let [post, setPosts] = useState([])
+    useEffect(() => {
+        getPosts()
+    })
+
   return (
     <div>
     Posts
