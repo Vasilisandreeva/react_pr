58e9115cf5f9904288e89c18c4d5921494ad3286
Vasya <vasya@gmail.com>
2021-09-10 19:34:14 +0300

    qqq

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import './App.css';
+import {useReducer} from "react";
+
+const reduser = (state, action) => {
+    switch (action) {
+        case '+':
+        return state + 10;
+        case '-':
+        return state - 2;
+        default:
+            return state
+    }
+}
 
 function App() {
+    let [state, dispatch] = useReducer(reduser, 0);
+    console.log(state)
+
   return (
     <div>
+      <h1>State 1 - {state}</h1>
+        <button onClick={() => dispatch('+')}>inc</button>
+        <button onClick={() => dispatch('-')}>dec</button>
     </div>
   );
 }
