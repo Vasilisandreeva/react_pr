355f980a54f490c1f428d78482759d46c521bdbd
feden2906 <feden2906@gmail.com>
2021-09-12 16:48:53 +0300

    little fix

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
 import {useReducer} from "react";
 
 const reduser = (state = {a: 0, b: 0, c: 0}, action) => {
-if (action.obj === '1' && action.diya === '+'){
-  return {...state, a: state.a++}
-} else if (action.obj === '1' && action.diya === '-') {
-  return {...state, a: state.a--}
-} else if (action.obj === '2' && action.diya === '+') {
-  return {...state, b: state.b++}
-} else if (action.obj === '2' && action.diya === '-') {
-  return {...state, b: state.b--}
-} else if (action.obj === '3' && action.diya === '+') {
-  return {...state, c: state.c++}
-} else if (action.obj === '3' && action.diya === '-') {
-  return {...state, c: state.c--}
+if (action.obj === '1'){
+  return {...state, a: state.a+action.diya}
+} else if (action.obj === '2') {
+  return {...state, b: state.b+action.diya}
+} else if (action.obj === '3') {
+  return {...state, c: state.c+action.diya}
 }}
 
 function App() {
-  let [state, dispatch] = useReducer(reduser, {a: 0, b: 0, c: 0})
-  let {a, b, c} = state;
+  const [state, dispatch] = useReducer(reduser, {a: 0, b: 0, c: 0})
+  const {a, b, c} = state;
 
   return (
     <div>
       <h1>State 1 - {a}</h1>
-      <button onClick={() => dispatch({obj: '1', diya: '+'})}>inc</button>
-        <button onClick={() => dispatch({obj: '1', diya: '-'})}>dec</button>
+      <button onClick={() => dispatch({obj: '1', diya: 1})}>inc</button>
+        <button onClick={() => dispatch({obj: '1', diya: -1})}>dec</button>
         <h1>State 1 - {b}</h1>
-        <button onClick={() => dispatch({obj: '2', diya: '+'})}>inc</button>
-        <button onClick={() => dispatch({obj: '2', diya: '-'})}>dec</button>
+        <button onClick={() => dispatch({obj: '2', diya: 1})}>inc</button>
+        <button onClick={() => dispatch({obj: '2', diya: -1})}>dec</button>
         <h1>State 1 - {c}</h1>
-        <button onClick={() => dispatch({obj: '3', diya: '+'})}>inc</button>
-        <button onClick={() => dispatch({obj: '3', diya: '-'})}>dec</button>
+        <button onClick={() => dispatch({obj: '3', diya: 1})}>inc</button>
+        <button onClick={() => dispatch({obj: '3', diya: -1})}>dec</button>
     </div>
   );
 }
